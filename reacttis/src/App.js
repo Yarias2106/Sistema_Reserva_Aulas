4c9747f4c2cb33b8b557c3e9356f16a1dc999e1d
HaroldZS <haroldzs@gmail.com>
2022-03-30 12:02:43 -0400

    Componente Footer

--- reacttis/src/App.js
+++ reacttis/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navigation from './Components/Navigation';
 import LoginForm from './Components/LoginForm';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <LoginForm />
+      <Footer />
     </div>
   );
 }
