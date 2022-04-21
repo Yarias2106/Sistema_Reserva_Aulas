054a792a114e97f8bf2ee943cb63676510cb91d9
HaroldZS <haroldzs@gmail.com>
2022-04-21 18:17:53 -0400

    Validación feecha frontend

--- Proyecto_Munay/static/Js/fecha.js
+++ Proyecto_Munay/static/Js/fecha.js
@@ -5,6 +5,7 @@ let month = date.getMonth() + 1;
 let year = date.getFullYear();
 let monthMax;
 let yearMax;
+let dayMax;
 
 if(month < 10){
   date = `${year}-0${month}-${day}`;
@@ -16,22 +17,45 @@ document.getElementById('Fecha').value = date;
 document.getElementById('Fecha').min = date;
 
 let dateMax = new Date();
-console.log(dateMax.getDate() + 13);
-dateMax = date.setDate(date.getDate()+5);
 
+ if(month =='01'){
+    if(year%4==0 && (year%100!=0 || year%400==0)){
+        if(day>=30){
+            dayMax = 29;
+        }else{
+            dayMax = day;
+        }
+    }else{
+        if(day>=29){
+            dayMax = 28;
+        }else{
+            dayMax = day;
+        }
+    }
+ }else{
+    if(month == '03' || month == '05' || month == '08' || month == '10'){
+        if(day==31){
+            dayMax= 30;
+        }else{
+            dayMax= day;
+        }
+    }else{
+        dayMax = day;
+    }
+ }
 
-// if(month==12){
-//     monthMax = 1;
-//     yearMax = year + 1; 
-// }else{
-//     monthMax = month + 1;
-//     yearMax = year;
-// }
+ if(month==12){
+    monthMax = 1;
+    yearMax = year + 1;     
+}else{
+    monthMax = month + 1;
+    yearMax = year;
+}
 
-// if(monthMax < 10){
-//     dateMax = `${yearMax}-0${monthMax}-${day}`;
-//   }else{
-//     dateMax = `${yearMax}-${monthMax}-${day}`;
-//   }
+if(monthMax < 10){
+    dateMax = `${yearMax}-0${monthMax}-${dayMax}`;
+  }else{
+    dateMax = `${yearMax}-${monthMax}-${dayMax}`;
+  }
 
   document.getElementById('Fecha').max = dateMax;
