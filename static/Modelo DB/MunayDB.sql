dcf54a2a65c259bf1e894a5b2c04bb0e719e70af
Ivan <monzonyujraivanfranklin@gmail.com>
2022-05-15 23:52:47 -0400

    Refactorizacion Problemas de cache y solucion errores de merge

--- static/Modelo DB/MunayDB.sql
+++ static/Modelo DB/MunayDB.sql
@@ -18,7 +18,9 @@
 --
 -- Table structure for table `auth_group`
 --
-
+DROP Database munaydb;
+CREATE Database munaydb;
+USE munaydb;
 DROP TABLE IF EXISTS `auth_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
