81eefdb7cfcf67a37d54d81f02c9062d9367f3b4
HaroldZS <haroldzs@gmail.com>
2022-05-19 23:11:43 -0400

    Reservas admin estilo de botones

--- static/Js/cambiarTitulo.js
+++ static/Js/cambiarTitulo.js
@@ -1,9 +1,15 @@
 let titleR = document.getElementById("tituloReservaAdmin");
+let reserT = document.getElementById("home-tab");
+let UrgenT = document.getElementById("profile-tab");
 
 function cambiarTitulo1(){
     titleR.textContent = "Reservas por orden de llegada";
+    reserT.className = "nav-link active";
+    UrgenT.className = "nav-link text-white";
 }
 
 function cambiarTitulo2(){
     titleR.textContent = "Reservas por Urgencia";
+    reserT.className = "nav-link text-white";
+    UrgenT.className = "nav-link active";
 }
