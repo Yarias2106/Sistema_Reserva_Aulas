2cdaff99e70fb2bbbc88aed07d67513f59d86ffc
HaroldZS <haroldzs@gmail.com>
2022-05-22 23:11:25 -0400

    mensaje de reservas vacias

--- static/Js/cambiarTitulo.js
+++ static/Js/cambiarTitulo.js
@@ -1,15 +1,36 @@
 let titleR = document.getElementById("tituloReservaAdmin");
 let reserT = document.getElementById("home-tab");
 let UrgenT = document.getElementById("profile-tab");
+let existeN = document.getElementById("normal");
+let existeU = document.getElementById("urgenciaT");
+let vacio = document.getElementById("vacio");
+
+if(existeN){
+    console.log("Hay reservas normales");
+}else{
+    vacio.textContent = "No hay reservas";
+}
 
 function cambiarTitulo1(){
     titleR.textContent = "Reservas por orden de llegada";
     reserT.className = "nav-link active";
     UrgenT.className = "nav-link text-white";
+
+    if(existeN){
+        vacio.textContent = "";
+    }else{
+        vacio.textContent = "No hay reservas";
+    }
 }
 
 function cambiarTitulo2(){
     titleR.textContent = "Reservas por Urgencia";
     reserT.className = "nav-link text-white";
     UrgenT.className = "nav-link active";
+
+    if(existeU){
+        vacio.textContent = "";
+    }else{
+        vacio.textContent = "No hay reservas de urgencia";
+    }
 }
