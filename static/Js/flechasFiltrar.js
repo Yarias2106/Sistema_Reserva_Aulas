87e9d35419c91ff994dea37e5e07e2d2d3b7281a
Clever <arnez.4292@gmail.com>
2022-05-26 22:23:21 -0400

    Hover, Icon flechas de filtros y buscar

---
+++ static/Js/flechasFiltrar.js
@@ -0,0 +1,24 @@
+
+document.getElementById('iconRow').addEventListener('click', function () {
+    var icon0 = document.getElementById('icon');
+    icon0.classList.toggle('bi-arrow-down');
+    icon0.classList.toggle('bi-arrow-up');
+});
+
+document.getElementById('iconRow1').addEventListener('click', function () {
+    var icon1 = document.getElementById('icon1');
+    icon1.classList.toggle('bi-arrow-down');
+    icon1.classList.toggle('bi-arrow-up');
+});
+
+document.getElementById('iconRow2').addEventListener('click', function () {
+    var icon2 = document.getElementById('icon2');
+    icon2.classList.toggle('bi-arrow-down');
+    icon2.classList.toggle('bi-arrow-up');
+});
+
+document.getElementById('iconRow3').addEventListener('click', function () {
+    var icon3 = document.getElementById('icon3');
+    icon3.classList.toggle('bi-arrow-down');
+    icon3.classList.toggle('bi-arrow-up');
+});
