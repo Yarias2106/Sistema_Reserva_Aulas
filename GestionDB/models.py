098caf5945e42eb74f946bda7f02e5f5f3640314
Ivan <monzonyujraivanfranklin@gmail.com>
2022-06-10 22:40:18 -0400

    Cambio modelo DB se agrego la columna Dia_Horarios

--- GestionDB/models.py
+++ GestionDB/models.py
@@ -1,4 +1,5 @@
 from operator import truediv
+from pyexpat import model
 from django.db import models
 from django.db.models.deletion import CASCADE
 # Create your models here.
@@ -64,6 +65,7 @@ class Grupo(models.Model):
     Cod_Materia= models.ForeignKey(Materia,on_delete=CASCADE,null=True)
     Cant_Inscritos = models.IntegerField(null=True)
     Cod_Grupo= models.CharField(max_length=60,null=True)
+    Dia_Horarios = models.CharField(max_length=250, null=True)
     
     def __str__(self):
         return self.Cod_Docente
