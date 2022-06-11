7c3b4af3b0feb8b28afde3693cf7dca93d33af0f
HaroldZS <haroldzs@gmail.com>
2022-06-10 22:00:51 -0400

    Error de horario y tabla parametros corregidos

--- Proyecto_Munay/settings.py
+++ Proyecto_Munay/settings.py
@@ -136,7 +136,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-es'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Santiago'
 
